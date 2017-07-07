2980649f221ec5273ae3f17fbc30aecb518397f2
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2017-07-07 01:24:37 -0400

    AdminLTE

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/admin', function(req, res, next) {
+  res.render('admin', { title: 'Express' });
+});
+
 module.exports = router;
